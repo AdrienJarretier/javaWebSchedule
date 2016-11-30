369479459f2f8b80709d48bd9722b1a21030a3b5
lauriehouseaux <lma.houseaux@gmail.com>
2016-11-30 23:34:47 +0100

    Add method verify_login in userADO

--- webSchedule/src/java/model/UserDao.java
+++ webSchedule/src/java/model/UserDao.java
@@ -5,6 +5,11 @@
  */
 package model;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
@@ -20,7 +25,19 @@ public class UserDao {
         this.myDataSource = DS.getDataSource();
     }
 
-    public User verify_login(String login, String mp){
+    public User verify_login(String login, String password) throws SQLException, NoSuchAlgorithmException{
+        String sql = "SELECT MP FROM STAFF WHERE EMAIL = ? ";
+        Connection connection = myDataSource.getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        
+        stmt.setString(1,login);
+        ResultSet rs = stmt.executeQuery();
+        
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] tab_log = login.getBytes();
+        byte[] log_encrypt = md.digest(tab_log);
+        
+       
         
     }
 }
