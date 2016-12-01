71b9ce3d4fa44c968b4f716a669eea129d22599f
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-12-01 09:09:57 +0100

    password hash

--- webSchedule/src/java/model/UserDao.java
+++ webSchedule/src/java/model/UserDao.java
@@ -7,11 +7,8 @@ package model;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import javax.sql.DataSource;
+import java.sql.*;
+import java.util.Arrays;
 
 /**
  *
@@ -25,19 +22,61 @@ public class UserDao {
         this.myDataSource = DS.getDataSource();
     }
 
-    public User verify_login(String login, String password) throws SQLException, NoSuchAlgorithmException{
-        String sql = "SELECT MP FROM STAFF WHERE EMAIL = ? ";
+    public User verify_login(String login, String password) throws SQLException, NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] passwordBytes = password.getBytes();
+        byte[] passwordHashed = md.digest(passwordBytes);
+
+        String sql = "SELECT password FROM testpasshash";
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
-        stmt.setString(1,login);
+
         ResultSet rs = stmt.executeQuery();
-        
+
+        byte[] passwordStored = null;
+
+        if (rs.next()) {
+            passwordStored = rs.getBytes(1);
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        if (Arrays.equals(passwordHashed, passwordStored)  {
+
+            return new User();
+        } else {
+            // TODO throw exception
+        }
+    }
+    
+    public int addStaff(String email, String first_name, String last_name, String password, boolean is_admin) throws NoSuchAlgorithmException, SQLException {
+
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-        byte[] tab_log = login.getBytes();
-        byte[] log_encrypt = md.digest(tab_log);
+        byte[] passwordBytes = password.getBytes();
+        byte[] passwordHashed = md.digest(passwordBytes);
+
+        String sql = "INSERT INTO staff(email, first_name, last_name, password, is_admin) VALUES(?, ?, ?, ?, ?)";
+        Connection connection = myDataSource.getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
         
-       
+        stmt.setBytes(4, passwordHashed);
+        stmt.execute();
+
+        ResultSet generatedKeys = stmt.getGeneratedKeys();
         
+        int generatedId = 0;
+        
+        if (generatedKeys.next()) {
+            generatedId = generatedKeys.getInt(1);
+        }
+
+        generatedKeys.close();
+        stmt.close();
+        connection.close();
+
+        return generatedId;
     }
 }
