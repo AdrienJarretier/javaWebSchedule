f0119f10d9346ae1b2437289c2db293210f3e5e4
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-12-01 17:44:38 +0100

    new class Class_room

---
+++ webSchedule/src/java/model/Class_room.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Jarretier Adrien "jarretier.adrien@gmail.com"
+ */
+public class Class_room {
+    
+    private int id;
+    private String building;
+    private int room_number;
+    private int capacity;
+
+    public Class_room(int id, String building, int room_number, int capacity) {
+        this.id = id;
+        this.building = building;
+        this.room_number = room_number;
+        this.capacity = capacity;
+    }
+    
+}
