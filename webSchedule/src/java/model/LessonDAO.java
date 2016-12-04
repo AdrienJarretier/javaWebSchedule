6ecb7997a3a94f3e2d1a98ccb6dac066f5d7afb9
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-12-04 21:56:06 +0100

    get by id with all entities class_room, staff and participants

--- webSchedule/src/java/model/LessonDAO.java
+++ webSchedule/src/java/model/LessonDAO.java
@@ -10,12 +10,12 @@ import javax.sql.DataSource;
  * @author Jarretier Adrien "jarretier.adrien@gmail.com"
  */
 public class LessonDAO {
-    
+
     private final DataSource myDataSource;
-    
+
     private static final String LESSON_TABLE = "lesson";
     private static final String PARTICIPANTS_TABLE = "lesson_participants";
-    
+
     public LessonDAO() throws SQLException {
         this.myDataSource = DS.getDataSource();
     }
@@ -30,44 +30,44 @@ public class LessonDAO {
         String sql = "INSERT INTO " + LESSON_TABLE
                 + "(time_start, time_end, title, class_room_id, teacher_id)"
                 + " VALUES (?,?,?,?,?)";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        
+
         stmt.setTimestamp(1, time_start);
         stmt.setTimestamp(2, time_end);
         stmt.setString(3, title);
         stmt.setInt(4, class_room_id);
         stmt.setInt(5, teacher_id);
         stmt.execute();
-        
+
         ResultSet generatedKeys = stmt.getGeneratedKeys();
-        
+
         int generatedId = 0;
-        
+
         if (generatedKeys.next()) {
             generatedId = generatedKeys.getInt(1);
         }
-        
+
         generatedKeys.close();
         stmt.close();
-        
+
         sql = "INSERT INTO " + PARTICIPANTS_TABLE
                 + "(degree_id, lesson_id)"
                 + " VALUES (?, ?)";
-        
+
         stmt = connection.prepareStatement(sql);
-        
+
         for (Degree participant : participants) {
-            
+
             stmt.setInt(1, participant.getId());
             stmt.setInt(2, generatedId);
             stmt.execute();
         }
-        
+
         stmt.close();
         connection.close();
-        
+
         return generatedId;
     }
 
@@ -79,21 +79,21 @@ public class LessonDAO {
      */
     public void remove(int lessonId) throws SQLException {
         String sql = "DELETE FROM " + PARTICIPANTS_TABLE + " WHERE lesson_id = ?";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         stmt.setInt(1, lessonId);
         stmt.execute();
         stmt.close();
-        
+
         sql = "DELETE FROM " + LESSON_TABLE + " WHERE id = ?";
         stmt = connection.prepareStatement(sql);
-        
+
         stmt.setInt(1, lessonId);
         stmt.execute();
         stmt.close();
-        
+
         connection.close();
     }
 
@@ -106,26 +106,26 @@ public class LessonDAO {
      */
     public void remove(Timestamp time_start, Class_room class_room) throws SQLException {
         String sql = "SELECT id FROM " + LESSON_TABLE + " WHERE time_start = ? and class_room_id = ?";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         stmt.setTimestamp(1, time_start);
         stmt.setInt(2, class_room.getId());
-        
+
         ResultSet rs = stmt.executeQuery();
-        
+
         int id = -1;
-        
+
         if (rs.next()) {
-            
+
             id = rs.getInt(1);
-            
+
         }
-        
+
         stmt.close();
         connection.close();
-        
+
         remove(id);
     }
 
@@ -138,26 +138,26 @@ public class LessonDAO {
      */
     public void remove(Timestamp time_start, Staff teacher) throws SQLException {
         String sql = "SELECT id FROM " + LESSON_TABLE + " WHERE time_start = ? and teacher_id = ?";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         stmt.setTimestamp(1, time_start);
         stmt.setInt(2, teacher.getId());
-        
+
         ResultSet rs = stmt.executeQuery();
-        
+
         int id = -1;
-        
+
         if (rs.next()) {
-            
+
             id = rs.getInt(1);
-            
+
         }
-        
+
         stmt.close();
         connection.close();
-        
+
         remove(id);
     }
 
@@ -174,40 +174,40 @@ public class LessonDAO {
                 + "title = ?, "
                 + "class_room_id = ?, "
                 + "teacher_id = ?";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         stmt.setTimestamp(1, lesson.getTimeStart());
         stmt.setTimestamp(2, lesson.getTimeEnd());
         stmt.setString(3, lesson.getTitle());
         stmt.setInt(4, lesson.getClass_room().getId());
         stmt.setInt(5, lesson.getTeacher().getId());
         stmt.execute();
-        
+
         stmt.execute();
         stmt.close();
-        
+
         sql = "UPDATE " + PARTICIPANTS_TABLE + " SET degree_id = ?, "
                 + "time_end = ?, "
                 + "title = ?, "
                 + "class_room_id = ?, "
                 + "teacher_id = ?";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         stmt.setTimestamp(1, lesson.getTimeStart());
         stmt.setTimestamp(2, lesson.getTimeEnd());
         stmt.setString(3, lesson.getTitle());
         stmt.setInt(4, lesson.getClass_room().getId());
         stmt.setInt(5, lesson.getTeacher().getId());
         stmt.execute();
-        
+
         stmt.execute();
         stmt.close();
         connection.close();
-        
+
         connection.close();
     }
 
@@ -219,80 +219,82 @@ public class LessonDAO {
      * @throws DAOException
      */
     public Lesson getById(int lesson_id) throws SQLException, DAOException {
-        
+
         String sql
                 = " SELECT * FROM " + LESSON_TABLE
-                + " INNER JOIN class_room ON class_room.id=LESSON.CLASS_ROOM_ID "
-                + " INNER JOIN staff ON staff.id=LESSON.TEACHER_ID "
                 + " WHERE id = ?";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         stmt.setInt(1, lesson_id);
         ResultSet rs = stmt.executeQuery();
-        
+
         if (rs.next()) {
-            
-            int id = rs.getInt("id");
+
+            int id = rs.getInt(1);
             Timestamp start = rs.getTimestamp("time_start");
             Timestamp end = rs.getTimestamp("time_end");
             String title = rs.getString("title");
             int class_room_id = rs.getInt("class_room_id");
             int teacher_id = rs.getInt("teacher_id");
-            
-            Lesson lesson = new Lesson(id, start, end, title, class_room_id, teacher_id);
-            
+
+            Class_room class_room = (new Class_roomDAO()).getById(class_room_id);
+            Staff teacher = (new StaffDAO()).getById(teacher_id);
+            ArrayList<Degree> participants = (new DegreeDAO()).getParticipants(lesson_id);
+
+            Lesson lesson = new Lesson(id, start, end, title, class_room, teacher, participants);
+
             rs.close();
             stmt.close();
             connection.close();
-            
+
             return lesson;
-            
+
         } else {
-            
+
             rs.close();
             stmt.close();
             connection.close();
-            
+
             throw new DAOException("this id does not match any lesson");
-            
+
         }
     }
-    
+
     private ArrayList<Lesson> getLessons(PreparedStatement stmt) throws SQLException {
-        
+
         ResultSet rs = stmt.executeQuery();
-        
+
         ArrayList<Lesson> lessons = new ArrayList<>();
-        
+
         while (rs.next()) {
-            
+
             int lesson_id = rs.getInt(1);
             Timestamp start = rs.getTimestamp("time_start");
             Timestamp end = rs.getTimestamp("time_end");
             String title = rs.getString("title");
-            
+
             int class_room_id = rs.getInt("class_room_id");
             String building = rs.getString("building");
             int room_nb = rs.getInt("room_nb");
             int capacity = rs.getInt("capacity");
-            
+
             Class_room class_room = new Class_room(class_room_id, building, room_nb, capacity);
-            
+
             int teacher_id = rs.getInt("teacher_id");;
             String email = rs.getString("email");
             String first_name = rs.getString("first_name");
             String last_name = rs.getString("last_name");
-            
+
             Staff teacher = new Staff(teacher_id, email, first_name, last_name, Boolean.FALSE);
-            
+
             lessons.add(new Lesson(lesson_id, start, end, title, class_room, teacher, (new DegreeDAO()).getParticipants(lesson_id)));
-            
+
         }
-        
+
         rs.close();
-        
+
         return lessons;
     }
 
@@ -304,21 +306,21 @@ public class LessonDAO {
      * @throws java.sql.SQLException
      */
     public ArrayList<Lesson> getSchedule() throws SQLException {
-        
+
         String sql
                 = " SELECT * FROM " + LESSON_TABLE
                 + " INNER JOIN class_room ON class_room.id=LESSON.CLASS_ROOM_ID "
                 + " INNER JOIN staff ON staff.id=LESSON.TEACHER_ID "
                 + " ORDER BY time_start";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         ArrayList<Lesson> lessons = getLessons(stmt);
-        
+
         stmt.close();
         connection.close();
-        
+
         return lessons;
     }
 
@@ -331,26 +333,26 @@ public class LessonDAO {
      * @throws java.sql.SQLException
      */
     public ArrayList<Lesson> getSchedule(Staff teacher) throws SQLException {
-        
+
         String sql
                 = " SELECT * FROM " + LESSON_TABLE
                 + " INNER JOIN class_room ON class_room.id=LESSON.CLASS_ROOM_ID "
                 + " INNER JOIN staff ON staff.id=LESSON.TEACHER_ID "
                 + " WHERE teacher_id = ? "
                 + " ORDER BY time_start";
-        
+
         Connection connection = myDataSource.getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
-        
+
         stmt.setInt(1, teacher.getId());
-        
+
         ArrayList<Lesson> lessons = getLessons(stmt);
-        
+
         stmt.close();
         connection.close();
-        
+
         return lessons;
-        
+
     }
-    
+
 }
